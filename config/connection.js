dd50d51da35fec38406d3c59ec74422a9ebc6fe7
Chyna Davis <chyna.davis11@gmail.com>
2020-05-04 17:48:39 -0700

    update connection.js connection info for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // set up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "r00tPa$$",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    // port: 3306,
+    user: "root",
+    password: "r00tPa$$",
+    database: "burgers_db"
+  });
+}
 
 // make connection
 connection.connect(function(err) {
